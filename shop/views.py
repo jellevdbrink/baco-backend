8c52c39a548b8d65319452d60a2bbfe33b82807d
Jelle <jelle@famvandenbrink.net>
2025-09-18 14:26:09 +0200

    add option to filter products on category

--- shop/views.py
+++ shop/views.py
@@ -1,33 +1,38 @@
 from rest_framework import viewsets, mixins
 from .models import Team, TeamMember, Category, Product, Order
 from .serializers import (
-    TeamSerializer, TeamMemberSerializer, CategorySerializer,
-    ProductSerializer, OrderSerializer
+  TeamSerializer, TeamMemberSerializer, CategorySerializer,
+  ProductSerializer, OrderSerializer
 )
 
 class TeamViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Team.objects.all()
-    serializer_class = TeamSerializer
+  queryset = Team.objects.all()
+  serializer_class = TeamSerializer
 
 
 class TeamMemberViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = TeamMember.objects.all()
-    serializer_class = TeamMemberSerializer
+  queryset = TeamMember.objects.all()
+  serializer_class = TeamMemberSerializer
 
 
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
+  queryset = Category.objects.all()
+  serializer_class = CategorySerializer
 
 
 class ProductViewSet(viewsets.ReadOnlyModelViewSet):
+  queryset = Product.objects.all()
+  serializer_class = ProductSerializer
+
+  def get_queryset(self):
     queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+    category_id = self.request.query_params.get('category')
+    if category_id is not None:
+      queryset = queryset.filter(category_id=category_id)
+    return queryset
 
 
-class OrderViewSet(mixins.CreateModelMixin,
-                   mixins.ListModelMixin,
-                   mixins.RetrieveModelMixin,
-                   viewsets.GenericViewSet):
-    queryset = Order.objects.all().select_related("by").prefetch_related("items__product")
-    serializer_class = OrderSerializer
+class OrderViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, 
+                   mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+  queryset = Order.objects.all().select_related("by").prefetch_related("items__product")
+  serializer_class = OrderSerializer
